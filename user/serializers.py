1a40969bd694546288279d61eb05bf4105a4dce7
iycel <ismailycel@gmail.com>
2021-11-29 01:20:36 +0300

    feat: login/register %50

---
+++ user/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers, validators
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password  # passwordun validate olması için import ettik
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True, validators=[validators.UniqueValidator(queryset=User.objects.all())])
+    username = serializers.CharField(required=True, validators=[validators.UniqueValidator(queryset=User.objects.all())])  # username unique olması için
+    first_name = serializers.CharField(required=True)  # isim girmenin required olması için yeniden yazdık
+    last_name = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password], style={'input_type':'password'})
+    password2 = serializers.CharField(write_only=True, required=True, style={'input_type':'password'})  # Passwordlarını girerken gözükmemesi için style={'input_type':'password'} yazdık
+
+    class Meta :
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'password2'
+        ]
+
+        extra_kwargs = {
+            'password' : {'write_only' : True},  # Passwordların database de gözükmemesi için extra argüman ekledim
+            'password2': {'write_only' : True}
+        }
+
+        # User modelimin fieldlerinda password 2 olmadığı için create metodunu overwrite edeceğiz. Create metodunun içerisinde gelen validated data create metodunu çıkartıp userimizi create edeceğiz.
+    
+    def create(self, validated_data):  # self olacak ve gelen datayı validated_data diye alıyoruz (Object-level validation -- 2 ayrı fieldi karşılaştırdığımız için)
+        password = validated_data.get('password')
+        validated_data.pop('password2') # validated_data dan pop modeli ile password2 yi çıkarıyoruz
+        user = User.objects.create(**validated_data)  # validated_datadan userimizi create ettik
+        user.set_password(password) # passwordlarımız database e açık bir şekilde yazamıyoruz. Hash lenerek yazıyoruz. O yüzden user create ederken password ü ayrı set etmemiz lazım.
+        user.save()
+        return user
+
+    def validate(self, data):  # (Object-level validation -- 2 ayrı fieldi karşılaştırdığımız için)
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({'Password fields did not match'})
+        return data
--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView
+from .serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterApi(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
